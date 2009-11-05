a0d2a81418e33c47bd5d004f898662b0563ef07b
chatgris <chatgri@gmail.com>
2009-11-05 14:14:07 +0100

    Some path name

--- install.rb
+++ install.rb
@@ -1,7 +1,7 @@
 # Install hook code here
 puts "** Installing BookieWookie Plugin...." 
 
-src = File.join(RAILS_ROOT, '/vendor/plugins/Bookie-Wooky/lib/bookie_wookie.yml')
+src = File.join(RAILS_ROOT, '/vendor/plugins/Bookie-Wookie/lib/bookie_wookie.yml')
 config = File.join(RAILS_ROOT, '/config/bookie_wookie.yml')
 puts "** Copying config file into #{config} ...." 
 FileUtils.cp(src, config) unless File.exists? config
